2b1d7e69a2dbc07d145b193aba3c7f3b02cec757
james WONG <james.wong@yucolab.com>
2018-09-07 12:23:52 +0800

    Confirmed form fields for booking

--- js/jsRegistration.js
+++ js/jsRegistration.js
@@ -1,28 +1,30 @@
 $(function() {
     // INSERT YOUR JAVASCRIPT HERE
+   $(".numbers-row").append('<div class="inc button">+</div><div class="dec button">-</div>');
+   
+
+	  $(".button").on("click", function() {
+
+		var $button = $(this);
+		var oldValue = $button.parent().find("input").val();
+
+		if ($button.text() == "+") {
+		  var newVal = parseFloat(oldValue) + 1;
+		} else {
+		 // Don't allow decrementing below zero
+		  if (oldValue > 0) {
+			var newVal = parseFloat(oldValue) - 1;
+		  } else {
+			newVal = 0;
+		  }
+		}
+
+		$button.parent().find("input").val(newVal);
+
+	  });
+
+	  
    $(window).bind('load resize', function() {
-
-      $(".numbers-row").append('<div class="inc button">+</div><div class="dec button">-</div>');
-
-      $(".button").on("click", function() {
-
-        var $button = $(this);
-        var oldValue = $button.parent().find("input").val();
-
-        if ($button.text() == "+") {
-          var newVal = parseFloat(oldValue) + 1;
-        } else {
-         // Don't allow decrementing below zero
-          if (oldValue > 0) {
-            var newVal = parseFloat(oldValue) - 1;
-          } else {
-            newVal = 0;
-          }
-        }
-
-        $button.parent().find("input").val(newVal);
-
-      });
 
 
    
